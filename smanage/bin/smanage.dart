70bbe93d6f347edc4d65c1a807b8dd03f3d749fb
pdnam2003 <nam.pd.2446@aptechlearning.edu.vn>
2026-01-02 16:06:29 +0700

    new

---
+++ smanage/bin/smanage.dart
@@ -0,0 +1,5 @@
+import 'package:smanage/smanage.dart' as smanage;
+
+void main(List<String> arguments) {
+  print('Hello world: ${smanage.calculate()}!');
+}
---
+++ smanage/test/smanage_test.dart
@@ -0,0 +1,8 @@
+import 'package:smanage/smanage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
